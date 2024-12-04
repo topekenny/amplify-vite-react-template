5592a30ae6139eb2b0941eb14c6bdb3a37a09bfc
topekenny <topekenny7@gmail.com>
2024-12-04 21:49:17 +0100

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -25,7 +25,7 @@ function App() {
       <button onClick={createTodo}>+ new</button>
       <ul>
         {todos.map((todo) => (
-          <li onclick={() => deleteTodo(todo:id)} key={todo.id}>{todo.content}</li>
+          <li onClick={() => deleteTodo(todo.id)} key={todo.id}>{todo.content}</li>
         ))}
       </ul>
       <div>
